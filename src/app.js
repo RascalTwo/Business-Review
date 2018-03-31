6fe21d16f4c7ed7f165baca2684ae4f29911c3b8
Vladimir Šterjoski <vladimir.sterjoski@gmail.com>
2018-03-31 15:59:57 +0200

    Add ESLint and update files

--- src/app.js
+++ src/app.js
@@ -3,49 +3,49 @@ import logo from './logo.svg';
 import './app.css';
 
 class App extends Component {
-	/**
-	 * Create an instance of the App.
-	 */
-	constructor(){
-		super();
+  /**
+   * Create an instance of the App.
+   */
+  constructor() {
+    super();
 
-		this.state = {
-			apiTime: null
-		};
-	}
+    this.state = {
+      apiTime: null
+    };
+  }
 
-	/**
-	 * Set the time from the API when the app mounts.
-	 */
-	componentDidMount(){
-		return this.updateTime();
-	}
+  /**
+   * Set the time from the API when the app mounts.
+   */
+  componentDidMount() {
+    return this.updateTime();
+  }
 
-	/**
-	 * Update 'state.apiTime' from the API.
-	 */
-	updateTime(){
-		return fetch('/api')
-			.then(r => r.json())
-			.then(response => {
-				this.setState({
-					apiTime: response.timestamp
-				});
-			});
-	}
+  /**
+   * Update 'state.apiTime' from the API.
+   */
+  updateTime() {
+    return fetch('/api')
+      .then(r => r.json())
+      .then((response) => {
+        this.setState({
+          apiTime: response.timestamp
+        });
+      });
+  }
 
-	/**
-	 * Render the component.
-	 */
-	render(){
-		return (
-			<div id="app">
-				<img src={logo} id="logo" alt="logo"/>
-				<div>{new Date(this.state.apiTime).toString()}</div>
-				<button onClick={() => this.updateTime()}>Update Time</button>
-			</div>
-		);
-	}
+  /**
+   * Render the component.
+   */
+  render() {
+    return (
+      <div id="app">
+        <img src={logo} id="logo" alt="logo" />
+        <div>{new Date(this.state.apiTime).toString()}</div>
+        <button onClick={() => this.updateTime()}>Update Time</button>
+      </div>
+    );
+  }
 }
 
 export default App;
