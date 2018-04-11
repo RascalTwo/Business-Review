2abecd0f277a4e606d4e914cc08440430b0defd2
Vladimir Šterjoski <vladimir.sterjoski@gmail.com>
2018-04-11 15:21:27 +0200

    Business Front-end

--- src/components/Businesses.js
+++ src/components/Businesses.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { PropTypes, businessShape } from '../proptypes';
+import BusinessCard from './BusinessCard';
+import '../static/Businesses.css';
 
 class Businesses extends React.Component {
   render() {
     return (
       <div id="businesses">
         <div className="container">
-          <p>{this.props.items.length} Businesses loaded, this is a placholder.</p>
+          <div className="businesses">
+            {this.props.items.map(business => (
+              <BusinessCard business={business} />
+            ))}
+          </div>
         </div>
       </div>
     );
